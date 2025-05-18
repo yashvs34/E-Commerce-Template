366390940f2d2e565f55d55dae2e28100107fa9f
root <root@Lenovo.>
2025-05-18 20:18:36 +0000

    Added more details to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 
 function Card ({id, it})
 {
+    const rating = Number(it.rating.toFixed(1));
     return (
         <>
             <div className="card">
@@ -9,11 +10,33 @@ function Card ({id, it})
                     <div className="cardTitle">
                         {it.title}
                     </div>
-                    <div className="itemPrice">
-                        {it.price}
+                    <div className="metaData">
+                        <div className="itemPrice">
+                            Price : ${it.price}
+                        </div>
+                        <div className="itemRating">
+                            Rating : {rating} / 5
+                        </div>
                     </div>
-                    <div className="itemRating">
-                        {it.rating}
+                    <div className="moreDetails">
+                        <div className="discount">
+                            Discount : {it.discountPercentage}%
+                        </div>
+                        <div>
+                            Stock : {it.stock}
+                        </div>
+                    </div>
+                    <div className="someMore">
+                        Minimum Order Quantity : {it.minimumOrderQuantity}
+                    </div>
+                    <div>
+                        Warranty : {it.warrantyInformation}
+                    </div>
+                    <div>
+                        Availabilty : {it.availabilityStatus}
+                    </div>
+                    <div>
+                        Return Policy : {it.returnPolicy}
                     </div>
                 </div>
             </div>
